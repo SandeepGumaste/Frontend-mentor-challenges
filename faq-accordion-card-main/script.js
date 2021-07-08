df70c5e24152560d5d3f3f6bb09ac0e70bd09649
Sandeep Gumaste <sandeepgumaste10@gmail.com>
2021-07-08 20:35:16 +0530

    desktop only

---
+++ faq-accordion-card-main/script.js
@@ -0,0 +1,13 @@
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq => {
+    faq.addEventListener('click',()=>{
+        removeActiveClasses();
+        faq.classList.toggle('active');
+    })
+});
+function removeActiveClasses(){
+    faqs.forEach(faq => {
+        faq.classList.remove('active') ;  
+    })
+}
+
