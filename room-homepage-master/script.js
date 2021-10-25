18548c92c97896630ea5113fbbdb5c2e5d8bf570
Sandeep Gumaste <sandeepgumaste10@gmail.com>
2021-10-25 15:21:48 +0530

    updated

--- room-homepage-master/script.js
+++ room-homepage-master/script.js
@@ -10,6 +10,8 @@ const headings = ['Discover innovative ways to decorate', 'We are available all 
 const para = ["We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love." ,"With stores all over the world, it's easy for you to find furniture for your home or place of business.Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.", "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."]
 
 let count=0
+let heroImgs = window.innerWidth<600? heroImagesMob: heroImagesDes;
+
 function heroChangeRight(){
     count = count>=2? 0: count+1;
     let heroImgs = window.innerWidth<600? heroImagesMob: heroImagesDes;
@@ -33,4 +35,3 @@ function hamClick(){
 function closeClick(){
     open.style.display = 'none';
 }
-console.log(window.innerWidth)
